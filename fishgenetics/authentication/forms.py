348daee16737a1570f7745c7b5dbab9b7a4c115d
charulpathak <charulpathak>
2023-04-19 12:13:04 +0530

    date and time widget done

--- fishgenetics/authentication/forms.py
+++ fishgenetics/authentication/forms.py
@@ -1,6 +1,6 @@
 from django.core import validators
 from django import forms
-
+from django.forms.widgets import CheckboxSelectMultiple
 from .models import fishbiodiversity_primary, fishbiodiversitysec, fishbioforexoticfish, fishtable, form, habitatparameter
 
 
@@ -8,18 +8,26 @@ class FormsRegistration(forms.ModelForm):
     class Meta:
         model = form
         fields = ("name_of_river","date","collector","locality","block","district","state","latitude","longitude","altitude")
+        widgets = {
+            'date' : forms.DateInput(attrs={'type':'date'})
+        }
 
 class HabitatparameterRegistration(forms.ModelForm):
     class Meta:
         model = habitatparameter
         fields = ("riverid","name_of_river","name_of_the_site","altitude_MSL","habitattype","width","depth","Substratepercent", "boulders","Cobbles","Pebbles","Gravels","Sand","SiltandClay","Aquaticpercent","Floating","Submerged","Emerged","Other","water_color","riparian_vegetation","water_velocity","temp_Air","temp_Water","pH","turbidity") 
 
-
 class Fishbiodiversity_primaryRegistration(forms.ModelForm):
     class Meta:
         model = fishbiodiversity_primary
         fields = ( "riverid","name_of_river","name_of_sampling_site","date_of_collection","collection_source","sampling_area","duration_of_fishing","time_of_fishing","total_catch","number_of_species" )
-               
+        widgets = {
+            'date_of_collection' : forms.DateInput(attrs={'type':'date'}),
+            'duration_of_fishing' : forms.TimeInput(attrs={'type': 'time'}),
+            'time_of_fishing' : forms.TimeInput(attrs={'type': 'time'}),
+          
+
+        }      
 
 
 class FishbiodiversitysecRegistration(forms.ModelForm):
